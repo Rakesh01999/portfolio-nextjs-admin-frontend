1bf9f754d298a1ee8f4452989ff9277deb7c7bc2
Rakesh Biswas <rbiswas01999@gmail.com>
2025-05-19 23:47:59 +0600

    professional photo added on dasboard

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -12,18 +12,24 @@ const DashboardPage = async () => {
       <div className="lg:ml-[130px]">
         <>
           <h1 className="text-4xl text-center mt-10">
-            Welcome To nafis ahamed
+            Welcome Rakesh Biswas 
           </h1>
           <h1 className="text-4xl text-center mt-10 mb-10">
-            Welcome To nafisahamed14@gmail.com
+            Personal Email : rbiswas01999@gmail.com
           </h1>
 
           <Image
-            src="https://i.postimg.cc/jjGLxfHz/nafis.jpg"
+            // src="https://i.postimg.cc/jjGLxfHz/nafis.jpg"
+            // src="https://i.postimg.cc/wMbTWVF0/rakesh1-Copy.jpg"
+            // src="https://i.postimg.cc/wM6PZY3k/Untitled-design-2-1.jpg"
+            src="https://i.postimg.cc/wTRczbJ5/rakesh-png.png"
             width={200}
+            // height={200}
             height={200}
             alt="image"
-            className="mx-auto rounded-full object-cover aspect-square"
+            // className="mx-auto rounded-full object-cover aspect-square"
+            className="mx-auto object-cover"
+            // className="mx-auto"
           />
         </>
       </div>
